bb1340ce92f3aeed5f3ac81ec18c7c7a8a4902fe
Rodrigo Ferreira <r.caneppele@gmail.com>
2016-06-17 13:36:45 -0300

    more tests for RoyalFlushChecker class

--- test/java/br/com/rcaneppele/poker/domain/logic/rank/RoyalFlushCheckerTest.java
+++ test/java/br/com/rcaneppele/poker/domain/logic/rank/RoyalFlushCheckerTest.java
@@ -39,5 +39,64 @@ public class RoyalFlushCheckerTest {
 			Assert.assertTrue(matches);
 		}
 	}
+	
+	@Test
+	public void handShouldNotBeRankedAsRoyalFlushWhenCardsAreNotAllOfTheSameSuit() {
+		for (CardSuit suit : CardSuit.values()) {
+			//The different Suit:
+			CardSuit different = Arrays.asList(CardSuit.values()).stream().filter(s -> !s.equals(suit)).findAny().get();
+			Card ace = new Card(different, CardValue.ACE);
+
+			Card king = new Card(suit, CardValue.KING);
+			Card queen = new Card(suit, CardValue.QUEEN);
+			Card jack = new Card(suit, CardValue.JACK);
+			Card ten = new Card(suit, CardValue.TEN);
+			
+			Set<Card> set = new HashSet<>(Arrays.asList(ace, king, queen, jack, ten));
+			Cards cards = new Cards(set);
+	
+			boolean matches = checker.matches(cards);
+			
+			Assert.assertFalse(matches);
+		}
+	}
+	
+	@Test
+	public void handShouldNotBeRankedAsRoyalFlushWhenCardsAreAllOfTheSameSuitButNotInAStraight() {
+		for (CardSuit suit : CardSuit.values()) {
+			//The out of Straight card:
+			Card three = new Card(suit, CardValue.THREE);
+
+			Card king = new Card(suit, CardValue.KING);
+			Card queen = new Card(suit, CardValue.QUEEN);
+			Card jack = new Card(suit, CardValue.JACK);
+			Card ten = new Card(suit, CardValue.TEN);
+			
+			Set<Card> set = new HashSet<>(Arrays.asList(three, king, queen, jack, ten));
+			Cards cards = new Cards(set);
+	
+			boolean matches = checker.matches(cards);
+			
+			Assert.assertFalse(matches);
+		}
+	}
+	
+	@Test
+	public void handShouldNotBeRankedAsRoyalFlushWhenCardsAreAllOfTheSameSuitButNotInTheCorrectStraightForRoyalFlush() {
+		for (CardSuit suit : CardSuit.values()) {
+			Card nine = new Card(suit, CardValue.NINE);
+			Card ten = new Card(suit, CardValue.TEN);
+			Card jack = new Card(suit, CardValue.JACK);
+			Card queen = new Card(suit, CardValue.QUEEN);
+			Card king = new Card(suit, CardValue.KING);
+			
+			Set<Card> set = new HashSet<>(Arrays.asList(nine, king, queen, jack, ten));
+			Cards cards = new Cards(set);
+	
+			boolean matches = checker.matches(cards);
+			
+			Assert.assertFalse(matches);
+		}
+	}
 
 }
