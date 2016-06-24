52611a3f4c2128364014a43c94da3e266cddab19
Rodrigo Ferreira <r.caneppele@gmail.com>
2016-06-24 11:26:09 -0300

    OnePairChecker algorithm and its tests

---
+++ src/java/br/com/rcaneppele/poker/domain/logic/rank/OnePairChecker.java
@@ -0,0 +1,29 @@
+package br.com.rcaneppele.poker.domain.logic.rank;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import br.com.rcaneppele.poker.domain.model.Card;
+import br.com.rcaneppele.poker.domain.model.CardValue;
+import br.com.rcaneppele.poker.domain.model.Cards;
+
+public class OnePairChecker implements RankChecker {
+
+	@Override
+	public boolean matches(Cards cards) {
+		Map<CardValue, List<Card>> groupedByValue = cards.getCards().stream().collect(Collectors.groupingBy(Card::getValue));
+		List<CardValue> values = cards.getCards().stream().map(Card::getValue).distinct().collect(Collectors.toList());
+		
+		return groupedByValue.size() == 4 
+				&& 
+				(groupedByValue.get(values.get(0)).size() == 2 && groupedByValue.get(values.get(1)).size() == 1 && groupedByValue.get(values.get(2)).size() == 1 && groupedByValue.get(values.get(3)).size() == 1)
+				||
+				(groupedByValue.get(values.get(0)).size() == 1 && groupedByValue.get(values.get(1)).size() == 2 && groupedByValue.get(values.get(2)).size() == 1 && groupedByValue.get(values.get(3)).size() == 1)
+				||
+				(groupedByValue.get(values.get(0)).size() == 1 && groupedByValue.get(values.get(1)).size() == 1 && groupedByValue.get(values.get(2)).size() == 2 && groupedByValue.get(values.get(3)).size() == 1)
+				||
+				(groupedByValue.get(values.get(0)).size() == 1 && groupedByValue.get(values.get(1)).size() == 1 && groupedByValue.get(values.get(2)).size() == 1 && groupedByValue.get(values.get(3)).size() == 2);
+	}
+
+}
---
+++ test/java/br/com/rcaneppele/poker/domain/logic/rank/OnePairCheckerTest.java
@@ -0,0 +1,72 @@
+package br.com.rcaneppele.poker.domain.logic.rank;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.rcaneppele.poker.domain.model.Card;
+import br.com.rcaneppele.poker.domain.model.CardSuit;
+import br.com.rcaneppele.poker.domain.model.CardValue;
+import br.com.rcaneppele.poker.domain.model.Cards;
+
+public class OnePairCheckerTest {
+	
+	private RankChecker checker;
+	
+	@Before
+	public void before() {
+		this.checker = new OnePairChecker();
+	}
+	
+	@Test
+	public void handShouldBeRankedAsOnePair() {
+		for (CardValue value : CardValue.values()) {
+			//The different Values:
+			CardValue firstDifferent = Arrays.asList(CardValue.values()).stream().filter(v -> !v.equals(value)).findAny().get();
+			CardValue secondDifferent = Arrays.asList(CardValue.values()).stream().filter(v -> !v.equals(value) && !v.equals(firstDifferent)).findAny().get();
+			CardValue thirdDifferent = Arrays.asList(CardValue.values()).stream().filter(v -> !v.equals(value) && !v.equals(firstDifferent) && !v.equals(secondDifferent)).findAny().get();
+			Card firstOfPair = new Card(CardSuit.HEARTS, firstDifferent);
+			Card secondOfPair = new Card(CardSuit.CLUBS, firstDifferent);
+			
+			Card firstOther = new Card(CardSuit.HEARTS, secondDifferent);
+			Card secondOther = new Card(CardSuit.DIAMONDS, thirdDifferent);
+			Card thirdOther = new Card(CardSuit.DIAMONDS, value);
+			
+			Set<Card> set = new HashSet<>(Arrays.asList(firstOfPair, secondOfPair, firstOther, secondOther, thirdOther));
+			Cards cards = new Cards(set);
+	
+			boolean matches = checker.matches(cards);
+			
+			Assert.assertTrue(matches);
+		}
+	}
+	
+	@Test
+	public void handShouldNotBeRankedAsOnePair() {
+		for (CardValue value : CardValue.values()) {
+			//The different Values:
+			CardValue firstDifferent = Arrays.asList(CardValue.values()).stream().filter(v -> !v.equals(value)).findAny().get();
+			CardValue secondDifferent = Arrays.asList(CardValue.values()).stream().filter(v -> !v.equals(value) && !v.equals(firstDifferent)).findAny().get();
+			CardValue thirdDifferent = Arrays.asList(CardValue.values()).stream().filter(v -> !v.equals(value) && !v.equals(firstDifferent) && !v.equals(secondDifferent)).findAny().get();
+			CardValue fourthDifferent = Arrays.asList(CardValue.values()).stream().filter(v -> !v.equals(value) && !v.equals(firstDifferent) && !v.equals(secondDifferent) && !v.equals(thirdDifferent)).findAny().get();
+
+			Card first = new Card(CardSuit.HEARTS, firstDifferent);
+			Card second = new Card(CardSuit.CLUBS, secondDifferent);
+			Card third = new Card(CardSuit.HEARTS, thirdDifferent);
+			Card fourth = new Card(CardSuit.DIAMONDS, fourthDifferent);
+			Card fifth = new Card(CardSuit.DIAMONDS, value);
+			
+			Set<Card> set = new HashSet<>(Arrays.asList(first, second, third, fourth, fifth));
+			Cards cards = new Cards(set);
+	
+			boolean matches = checker.matches(cards);
+			
+			Assert.assertFalse(matches);
+		}
+	}
+	
+}
